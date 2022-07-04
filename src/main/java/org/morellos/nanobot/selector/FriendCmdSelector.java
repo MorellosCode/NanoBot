d56cba8054164df9b0307316b90c4599909cb5b8
Robin Lang <robinlang@foxmail.com>
2022-07-04 16:22:59 +0800

    暂时注释未启用功能

--- src/main/java/org/morellos/nanobot/selector/FriendCmdSelector.java
+++ src/main/java/org/morellos/nanobot/selector/FriendCmdSelector.java
@@ -1,29 +1,30 @@
-package org.morellos.nanobot.selector;
-
-import net.mamoe.mirai.message.data.MessageChain;
-import org.morellos.nanobot.command.Command;
-import org.morellos.nanobot.command.FriendMessageCommand;
-import org.morellos.nanobot.core.NanoBot;
-import org.morellos.nanobot.utils.CommandUtils;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
-import java.util.Map;
-
-/**
- * 好友指令选择器
- *
- * @author Robin Lang
- * @since 2022/6/30
- */
-@Component
-public class FriendCmdSelector {
-
-    @Resource
-    private Map<String, FriendMessageCommand> friendCmdMap;
-
-    public Command getCommand(MessageChain messageChain) {
-        String key = CommandUtils.getCommandKey(messageChain);
-        return friendCmdMap.get(NanoBot.cmdMapping.get(key));
-    }
-}
+//package org.morellos.nanobot.selector;
+//
+//import net.mamoe.mirai.message.data.MessageChain;
+//import org.morellos.nanobot.command.Command;
+//import org.morellos.nanobot.command.FriendMessageCommand;
+//import org.morellos.nanobot.core.NanoBot;
+//import org.morellos.nanobot.utils.CommandUtils;
+//import org.springframework.stereotype.Component;
+//
+//import javax.annotation.Resource;
+//import java.util.Map;
+//import java.util.concurrent.ConcurrentHashMap;
+//
+///**
+// * 好友指令选择器
+// *
+// * @author Robin Lang
+// * @since 2022/6/30
+// */
+//@Component
+//public class FriendCmdSelector {
+//
+//    @Resource
+//    private Map<String, FriendMessageCommand> friendCmdMap;
+//
+//    public Command getCommand(MessageChain messageChain) {
+//        String key = CommandUtils.getCommandKey(messageChain);
+//        return friendCmdMap.get(NanoBot.cmdMapping.get(key));
+//    }
+//}
